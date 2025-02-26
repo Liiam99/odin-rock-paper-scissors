bfe49e42593ab768fbdf94c964bf2926c638d056
Liam <liam99mail@proton.me>
2025-02-26 19:01:45 +0100

    Finish interface

--- script.js
+++ script.js
@@ -1,48 +1,58 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const buttons = document.querySelectorAll('button');
-
     let humanScore = 0;
     let computerScore = 0;
+
+    const buttons = document.querySelectorAll('button');
 
     buttons.forEach((btn) => {
         btn.addEventListener('click', (e) => {
             const playerSelection = e.target.textContent.toLowerCase();
+            const computerSelection = getComputerChoice();
 
-            winner = playRound(playerSelection, getComputerChoice());
-
-            if (winner == 'human') {
+            const roundWinner = playRound(playerSelection, computerSelection);
+            if (roundWinner == 'human') {
                 humanScore++;
-            } else if (winner == 'computer') {
+                updateScore('human', humanScore);
+            } else if (roundWinner == 'computer') {
                 computerScore++;
+                updateScore('computer', computerScore);
+            }
+
+            const gameWinner = checkScore(humanScore, computerScore);
+            if (gameWinner) {
+                endGame(gameWinner, buttons);
+            } else {
+                addRoundMessage(roundWinner, playerSelection, computerSelection);
             }
         });
     });
 });
 
 function playRound(humanChoice, computerChoice) {
-    let result;
+    let winner;
 
     if (humanChoice == computerChoice) {
-        console.log('Tie.');
-
-        result = 'none';
+        winner = 'none';
     } else if (
             humanChoice == 'rock' && computerChoice == 'scissors'
             || humanChoice == 'paper' && computerChoice == 'rock'
             || humanChoice == 'scissors' && computerChoice == 'paper'
         ) {
-            humanChoice = capitaliseFirstLetter(humanChoice);
-            console.log(`You won the round: ${humanChoice} beats ${computerChoice}.`);
-
-            result = 'human';
+            winner = 'human';
     } else {
-        computerChoice = capitaliseFirstLetter(computerChoice);
-        console.log(`You lost the round: ${computerChoice} beats ${humanChoice}.`);
-
-        result = 'computer';
+        winner = 'computer';
     }
 
-    return result;
+    return winner;
+}
+
+
+function endGame(gameWinner, buttons) {
+    buttons.forEach((btn) => {
+        btn.disabled = true;
+    });
+
+    addGameEndMessage(gameWinner);
 }
 
 
@@ -64,6 +74,49 @@ function getComputerChoice() {
     }
 
     return choice;
+}
+
+
+function checkScore(humanScore, computerScore) {
+    if (humanScore == 5) {
+        return 'human';
+    } else if (computerScore == 5) {
+        return 'computer';
+    } else {
+        return null;
+    }
+}
+
+
+function updateScore(roundWinner, score) {
+    scoreDiv = document.querySelector(`.${roundWinner}-score`);
+    scoreDiv.textContent = score;
+}
+
+
+function addRoundMessage(roundWinner, humanSelection, computerSelection) {
+    humanSelection = capitaliseFirstLetter(humanSelection)
+    const message = document.querySelector('.message');
+
+    if (roundWinner == 'human') {
+        message.textContent = `You win the round. ${humanSelection} beats ${computerSelection}.`;
+    } else if (roundWinner == 'computer') {
+        message.textContent = `You lose the round. ${humanSelection} is beaten by ${computerSelection}.`;
+    } else {
+        message.textContent = 'Tie.';
+    }
+}
+
+
+function addGameEndMessage(gameWinner) {
+    const message = document.querySelector('.message');
+
+    if (gameWinner == 'human') {
+        message.textContent = 'You won! Refresh the site to play again.';
+    } else if (gameWinner == 'computer') {
+        message.textContent = 'You lost. Refresh the site to try again.'
+    }
+
 }
 
 
